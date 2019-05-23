4da18a1462bb2fdc84775a7f4c01e115159273b6
钟智坤 <1129419956@qq.com>
2019-05-23 16:33:42 +0800

    Update hello_world.go

--- code/ch1/hello/hello_world.go
+++ code/ch1/hello/hello_world.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	if len(os.Args) > 1 {
-		fmt.Println("Hello World", os.Args[1])
+		fmt.Println("Hello World", os.Args[1], os.Args[2])  // os.Args获取命令行参数
 	}
 }
+
+// 一定要使用 package main 但是对目录名没有什么要求
